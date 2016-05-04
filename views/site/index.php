eaa331228c107b97460134e3a39ebf193ed2b1d1
kristinabeg <kristina.beg@gmail.com>
2016-05-04 14:23:10 +0200

    commit by kika - promijenjen heading na pocetnoj stranici

--- views/site/index.php
+++ views/site/index.php
@@ -18,7 +18,7 @@ $this->title = ' Aplikacija za pracenje peroralne antikoagulantne terapije';
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Kika</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -28,7 +28,7 @@ $this->title = ' Aplikacija za pracenje peroralne antikoagulantne terapije';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Ines</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
